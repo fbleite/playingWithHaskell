58ab13ab8f40e1c7dce3878b5d7af3a9c7a82fb4
fbleite <feliperbleite@gmail.com>
2019-06-09 18:03:52 -0400

    Added Haskell files

---
+++ playground.hs
@@ -0,0 +1,15 @@
+doubleMe x = x + x  
+
+doubleThis x = x + x  
+
+
+tlvParser :: [Char] -> ([Char], [Char], [Char])
+tlvParser (t1:t2:l1:l2:v) = (t1:t2:[], l1:l2:[], v)
+
+
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"
