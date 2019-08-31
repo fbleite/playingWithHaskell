a704a57b499f670b54a87970403cd54070fbe27b
fbleite <feliperbleite@gmail.com>
2019-08-31 17:46:27 -0400

    Finalized simple interpreter, still needs refactoring

--- simpleInterpreter.hs
+++ simpleInterpreter.hs
@@ -4,44 +4,71 @@ import qualified Data.Map as DM (fromList, insert, lookup)
 type Registers = M.Map String Int
 -- ["mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a"]
 
--- simpleAssembler :: [String] -> Registers
--- simpleAssembler xs =  foldl (\reg instruction -> processInstruction reg instruction) (DM.fromList []) xs
-
-
 simpleAssembler :: [String] -> Registers
-simpleAssembler xs =  foldl (\reg instruction -> processInstruction reg instruction) (DM.fromList []) xs
-    
-processInstruction :: Registers -> String -> Registers
-processInstruction reg instruction 
-    | command == "mov" = processMov reg arguments
-    | command == "inc" = processInc reg arguments
-    | command == "dec" = processDec reg arguments
-    | command == "jnz" = DM.fromList[("a", 4)]
+simpleAssembler xs = processInstruction (DM.fromList []) xs 0
+
+processInstruction :: Registers -> [String] -> Int -> Registers
+processInstruction reg instructions currentInstruction
+    | currentInstruction == length instructions = reg
+    | command == "mov" = processInstruction newRegs instructions (currentInstruction + 1)
+    | command == "inc" = processInstruction newRegs instructions (currentInstruction + 1)
+    | command == "dec" = processInstruction newRegs instructions (currentInstruction + 1)
+    | command == "jnz" = processInstruction newRegs instructions (processJnz newRegs arguments currentInstruction)
     | otherwise = undefined
-    where parsedInstruction = words instruction
+    where parsedInstruction = words $ instructions !! currentInstruction
           command = head parsedInstruction
           arguments = tail parsedInstruction
+          newRegs 
+            | command == "mov" = processMov reg arguments
+            | command == "inc" = processInc reg arguments
+            | command == "dec" = processDec reg arguments
+            | command == "jnz" = reg
+            | otherwise = undefined
+
 
 processMov :: Registers -> [String] -> Registers
-processMov reg args = DM.insert regName value reg
+processMov regs args 
+    | all isAlpha value = DM.insert regName valueFromReg regs
+    | otherwise = DM.insert regName valueInt regs
     where regName = head args
-          value = read . last $ args
+          value = last $ args
+          valueInt = read value
+          valueFromReg = getRegisterValue regs value
+
+
+isAlpha :: Char -> Bool
+isAlpha c 
+    | c `elem` ['a'..'z'] = True
+    | c `elem` ['A'..'Z'] = True
+    | otherwise = False
 
 processInc :: Registers -> [String] -> Registers
-processInc reg args = processMov reg moveArgs
+processInc regs args = processMov regs moveArgs
     where regName = head args 
-          currentRegValue = case DM.lookup regName reg of
-                                Just n -> n
-                                Nothing -> error "Register not present"
+          currentRegValue = getRegisterValue regs regName
           newRegValueString =  show $ (currentRegValue + 1)
           moveArgs = [regName, newRegValueString]
 
 
 processDec :: Registers -> [String] -> Registers
-processDec reg args = processMov reg moveArgs
+processDec regs args = processMov regs moveArgs
     where regName = head args 
-          currentRegValue = case DM.lookup regName reg of
-                                Just n -> n
-                                Nothing -> error "Register not present"
+          currentRegValue = getRegisterValue regs regName
           newRegValueString =  show $ (currentRegValue - 1)
           moveArgs = [regName, newRegValueString]
+
+processJnz :: Registers -> [String] -> Int -> Int
+processJnz regs args currentInstruction = if(regToCheck /= 0)
+                                            then currentInstruction + skipCount
+                                            else currentInstruction + 1
+    where regName = head args 
+          skipCount = read . last $ args
+          regToCheck = getRegisterValue regs regName
+
+getRegisterValue :: Registers -> String -> Int
+getRegisterValue regs regName = case DM.lookup regName regs of
+                                    Just n -> n
+                                    Nothing -> error ("Register not present: " ++ regName)
+
+
+          
